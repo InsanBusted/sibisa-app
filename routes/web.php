d8529eb9880cae4c2693bfdb5d31f69456d4e91b
insan <insanbusted@gmail.com>
2024-12-13 14:31:41 +0700

    feat(mahasiswa): update delete mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(middleware: ['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'role:admin|mahasiswa'])->group(function () {
-    Route::get('/mahasiswa', [MahasiswaController::class, "index"])->name('mahasiswa');
-    Route::post('/mahasiswa', [MahasiswaController::class, "store"])->name('add-mahasiswa');
-    Route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, "update"])->name('edit-mahasiswa');
-});
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
+    // prodi
     Route::get('/prodi', [ProdiController::class, "index"])->name('prodi');
     Route::post('/prodi', [ProdiController::class, "store"])->name('add-prodi');
     Route::put('/prodi/{prodi}', [ProdiController::class, "update"])->name('edit-prodi');
     Route::delete('/prodi/{prodi}', [ProdiController::class, "destroy"])->name('delete-prodi');
+    
+    // mahasiswa
+    Route::get('/mahasiswa', [MahasiswaController::class, "index"])->name('mahasiswa');
+    Route::post('/mahasiswa', [MahasiswaController::class, "store"])->name('add-mahasiswa');
+    Route::put('/mahasiswa/{mahasiswa}', [MahasiswaController::class, "update"])->name('edit-mahasiswa');
+    Route::delete('/mahasiswa/{mahasiswa}', [MahasiswaController::class, "destroy"])->name('delete-mahasiswa');
 });
 
 
